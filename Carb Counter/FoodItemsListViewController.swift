2ceff6dcce7570aee1f5cbdcc97a6d6c03d8f028
dsnallfot <72826201+dsnallfot@users.noreply.github.com>
2024-06-19 15:17:41 +0200

    Add search and filter and sorting buttons in FoodItemsListView

--- Carb Counter/FoodItemsListViewController.swift
+++ Carb Counter/FoodItemsListViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 import CoreData
 
-class FoodItemsListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, AddFoodItemDelegate {
+class FoodItemsListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, AddFoodItemDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     var foodItems: [FoodItem] = []
+    var filteredFoodItems: [FoodItem] = []
+    var sortOption: SortOption = .nameAsc
+    var segmentedControl: UISegmentedControl!
+    var searchBar: UISearchBar!
+
+    enum SortOption {
+        case nameAsc, nameDesc, carbsAsc, carbsDesc, fatAsc, fatDesc, proteinAsc, proteinDesc
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,16 +28,73 @@ class FoodItemsListViewController: UIViewController, UITableViewDataSource, UITa
         fetchFoodItems()
         setupAddButton()
         setupNavigationBarTitle()
+        setupSearchBar()
+        setupSortSegmentedControl()
     }
 
     private func setupAddButton() {
-            let addButton = UIBarButtonItem(image: UIImage(systemName: "plus.circle"), style: .plain, target: self, action: #selector(navigateToAddFoodItem))
-            navigationItem.rightBarButtonItem = addButton
-        }
+        let addButton = UIBarButtonItem(image: UIImage(systemName: "plus.circle"), style: .plain, target: self, action: #selector(navigateToAddFoodItem))
+        navigationItem.rightBarButtonItem = addButton
+    }
 
     private func setupNavigationBarTitle() {
         // Set the title of the navigation bar
         title = "Food Items"
+    }
+
+    private func setupSearchBar() {
+        searchBar = UISearchBar()
+        searchBar.delegate = self
+        searchBar.placeholder = "Search Food Items"
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(searchBar)
+        
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+    }
+
+    private func setupSortSegmentedControl() {
+        let items = ["Name", "Carbs", "Fat", "Protein"]
+        segmentedControl = UISegmentedControl(items: items)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.addTarget(self, action: #selector(sortSegmentChanged(_:)), for: .valueChanged)
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(segmentedControl)
+        
+        NSLayoutConstraint.activate([
+            segmentedControl.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 8),
+            segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 8),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+
+    @objc private func sortSegmentChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            sortOption = sortOption == .nameAsc ? .nameDesc : .nameAsc
+        case 1:
+            sortOption = sortOption == .carbsAsc ? .carbsDesc : .carbsAsc
+        case 2:
+            sortOption = sortOption == .fatAsc ? .fatDesc : .fatAsc
+        case 3:
+            sortOption = sortOption == .proteinAsc ? .proteinDesc : .proteinAsc
+        default:
+            break
+        }
+        sortFoodItems()
     }
 
     @objc private func navigateToAddFoodItem() {
@@ -53,19 +118,42 @@ class FoodItemsListViewController: UIViewController, UITableViewDataSource, UITa
         let fetchRequest = NSFetchRequest<FoodItem>(entityName: "FoodItem")
         do {
             foodItems = try context.fetch(fetchRequest)
-            tableView.reloadData()
+            filteredFoodItems = foodItems
+            sortFoodItems()
         } catch {
             print("Failed to fetch food items: \(error)")
         }
     }
 
+    func sortFoodItems() {
+        switch sortOption {
+        case .nameAsc:
+            filteredFoodItems.sort { $0.name ?? "" < $1.name ?? "" }
+        case .nameDesc:
+            filteredFoodItems.sort { $0.name ?? "" > $1.name ?? "" }
+        case .carbsAsc:
+            filteredFoodItems.sort { $0.carbohydrates < $1.carbohydrates }
+        case .carbsDesc:
+            filteredFoodItems.sort { $0.carbohydrates > $1.carbohydrates }
+        case .fatAsc:
+            filteredFoodItems.sort { $0.fat < $1.fat }
+        case .fatDesc:
+            filteredFoodItems.sort { $0.fat > $1.fat }
+        case .proteinAsc:
+            filteredFoodItems.sort { $0.protein < $1.protein }
+        case .proteinDesc:
+            filteredFoodItems.sort { $0.protein > $1.protein }
+        }
+        tableView.reloadData()
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return foodItems.count
+        return filteredFoodItems.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FoodItemCell", for: indexPath)
-        let foodItem = foodItems[indexPath.row]
+        let foodItem = filteredFoodItems[indexPath.row]
         
         let name = foodItem.name ?? ""
         let carbs = String(format: "%.0f", foodItem.carbohydrates)
@@ -112,6 +200,7 @@ class FoodItemsListViewController: UIViewController, UITableViewDataSource, UITa
         do {
             try context.save()
             foodItems.remove(at: indexPath.row)
+            filteredFoodItems.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .automatic)
         } catch {
             print("Failed to delete food item: \(error)")
@@ -126,5 +215,21 @@ class FoodItemsListViewController: UIViewController, UITableViewDataSource, UITa
             addFoodItemVC.foodItem = foodItems[indexPath.row]
             navigationController?.pushViewController(addFoodItemVC, animated: true)
         }
+    }
+    // MARK: - UISearchBar Delegate
+
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            if searchText.isEmpty {
+                filteredFoodItems = foodItems
+            } else {
+                filteredFoodItems = foodItems.filter { $0.name?.lowercased().contains(searchText.lowercased()) ?? false }
+            }
+            sortFoodItems()
+            }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        filteredFoodItems = foodItems
+        sortFoodItems()
+        searchBar.resignFirstResponder()
     }
 }
