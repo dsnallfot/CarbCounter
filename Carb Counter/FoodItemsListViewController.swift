06153a4b39fccf24db21b25dcf0b824f81919a71
dsnallfot <72826201+dsnallfot@users.noreply.github.com>
2024-06-20 22:44:43 +0200

    Add import/export of csv fooditems

--- Carb Counter/FoodItemsListViewController.swift
+++ Carb Counter/FoodItemsListViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  FoodItemsListViewController.swift
 //  Carb Counter
@@ -7,6 +8,7 @@
 
 import UIKit
 import CoreData
+import UniformTypeIdentifiers
 
 class FoodItemsListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, AddFoodItemDelegate {
     
@@ -35,7 +37,10 @@ class FoodItemsListViewController: UIViewController, UITableViewDataSource, UITa
     
     private func setupAddButton() {
         let addButton = UIBarButtonItem(image: UIImage(systemName: "plus.circle"), style: .plain, target: self, action: #selector(navigateToAddFoodItem))
-        navigationItem.rightBarButtonItem = addButton
+        let exportButton = UIBarButtonItem(image: UIImage(systemName: "square.and.arrow.up"), style: .plain, target: self, action: #selector(exportFoodItemsToCSV))
+        let importButton = UIBarButtonItem(image: UIImage(systemName: "square.and.arrow.down"), style: .plain, target: self, action: #selector(importFoodItemsFromCSV))
+        navigationItem.leftBarButtonItems = [exportButton, importButton]
+        navigationItem.rightBarButtonItems = [addButton]
     }
     
     private func setupNavigationBarTitle() {
@@ -205,13 +210,12 @@ class FoodItemsListViewController: UIViewController, UITableViewDataSource, UITa
             }
             let yesAction = UIAlertAction(title: "Yes", style: .destructive) { _ in
                 self.deleteFoodItem(at: indexPath)
-                completionHandler(true) // Delete the item
+                completionHandler(true) //Delete the item
             }
             alertController.addAction(cancelAction)
             alertController.addAction(yesAction)
             self.present(alertController, animated: true, completion: nil)
         }
-        
         let editAction = UIContextualAction(style: .normal, title: "Edit") { (action, view, completionHandler) in
             self.editFoodItem(at: indexPath)
             completionHandler(true)
@@ -271,4 +275,119 @@ class FoodItemsListViewController: UIViewController, UITableViewDataSource, UITa
         sortFoodItems()
         searchBar.resignFirstResponder()
     }
+    
+    @objc private func exportFoodItemsToCSV() {
+        let fetchRequest: NSFetchRequest<FoodItem> = FoodItem.fetchRequest()
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let context = appDelegate.persistentContainer.viewContext
+        
+        do {
+            let foodItems = try context.fetch(fetchRequest)
+            let csvData = createCSV(from: foodItems)
+            saveCSV(data: csvData)
+        } catch {
+            print("Failed to fetch food items: \(error)")
+        }
+    }
+    
+    private func createCSV(from foodItems: [FoodItem]) -> String {
+        var csvString = "id,name,carbohydrates,carbsPP,fat,fatPP,netCarbs,netFat,netProtein,perPiece,protein,proteinPP\n"
+        
+        for item in foodItems {
+            let id = item.id?.uuidString ?? ""
+            let name = item.name ?? ""
+            let carbohydrates = item.carbohydrates
+            let carbsPP = item.carbsPP
+            let fat = item.fat
+            let fatPP = item.fatPP
+            let netCarbs = item.netCarbs
+            let netFat = item.netFat
+            let netProtein = item.netProtein
+            let perPiece = item.perPiece
+            let protein = item.protein
+            let proteinPP = item.proteinPP
+            
+            csvString += "\(id),\(name),\(carbohydrates),\(carbsPP),\(fat),\(fatPP),\(netCarbs),\(netFat),\(netProtein),\(perPiece),\(protein),\(proteinPP)\n"
+        }
+        
+        return csvString
+    }
+    
+    private func saveCSV(data: String) {
+        let fileName = "FoodItems.csv"
+        let path = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(fileName)
+        
+        do {
+            try data.write(to: path!, atomically: true, encoding: String.Encoding.utf8)
+            let activityViewController = UIActivityViewController(activityItems: [path!], applicationActivities: nil)
+            present(activityViewController, animated: true, completion: nil)
+        } catch {
+            print("Failed to create file: \(error)")
+        }
+    }
+    
+    @objc private func importFoodItemsFromCSV() {
+        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [UTType.commaSeparatedText])
+        documentPicker.delegate = self
+        present(documentPicker, animated: true, completion: nil)
+    }
+    
+    private func parseCSV(at url: URL) {
+        do {
+            let csvData = try String(contentsOf: url, encoding: .utf8)
+            let rows = csvData.components(separatedBy: "\n").filter { !$0.isEmpty }
+            let columns = rows[0].components(separatedBy: ";")
+
+            guard columns.count == 12 else {
+                print("CSV file does not have the correct format")
+                showAlert(title: "Import Failed", message: "CSV file does not have the correct format")
+                return
+            }
+
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+            let context = appDelegate.persistentContainer.viewContext
+
+            for row in rows[1...] {
+                let values = row.components(separatedBy: ";")
+                if values.count == 12 {
+                    let foodItem = FoodItem(context: context)
+                    foodItem.id = UUID(uuidString: values[0])
+                    foodItem.name = values[1]
+                    foodItem.carbohydrates = Double(values[2]) ?? 0.0
+                    foodItem.carbsPP = Double(values[3]) ?? 0.0
+                    foodItem.fat = Double(values[4]) ?? 0.0
+                    foodItem.fatPP = Double(values[5]) ?? 0.0
+                    foodItem.netCarbs = Double(values[6]) ?? 0.0
+                    foodItem.netFat = Double(values[7]) ?? 0.0
+                    foodItem.netProtein = Double(values[8]) ?? 0.0
+                    foodItem.perPiece = values[9] == "true"
+                    foodItem.protein = Double(values[10]) ?? 0.0
+                    foodItem.proteinPP = Double(values[11]) ?? 0.0
+                }
+            }
+
+            try context.save()
+            fetchFoodItems()
+            tableView.reloadData()
+            showAlert(title: "Import Successful", message: "Food items have been successfully imported.")
+        } catch {
+            print("Failed to read CSV file: \(error)")
+            showAlert(title: "Import Failed", message: "Failed to read CSV file: \(error)")
+        }
+    }
+
+    private func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
+extension FoodItemsListViewController: UIDocumentPickerDelegate {
+func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+guard let url = urls.first else { return }
+parseCSV(at: url)
+}
 }
